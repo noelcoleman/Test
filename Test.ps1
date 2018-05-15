707a2195f291030619c362769f38340c03a9086b
noelcoleman <noel.coleman@gmail.com>
2018-05-15 21:04:48 +0100

    Test 2
    This is a further test.

--- Test.ps1
+++ Test.ps1
@@ -1,3 +1,11 @@
-Resolve-DnsName -Name google.com -Server 1.1.1.1
+$DNSName = (Resolve-DnsName -Name google.com -Server 1.1.1.1).IPAddress[1]
+
+Write-Host $DNSName
+
+Test-NetConnection -ComputerName $DNSName -TraceRoute
+
+Get-NetAdapter -Name "*" | fl ifAlias
+
+
 
 
